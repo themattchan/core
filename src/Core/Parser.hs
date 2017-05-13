330537103cc021316ab4aaf620917f7e7c491de1
Matthew Chan <matt@themattchan.com>
2017-05-13 00:05:38 -0700

    wtf where are my intermediate operators

--- src/Core/Parser.hs
+++ src/Core/Parser.hs
@@ -4,6 +4,7 @@ import Data.Functor
 import Text.Parsec
 import Text.Parsec.Language
 import qualified Text.Parsec.Token as P
+import qualified Text.Parsec.Expr as P
 import Text.Parsec.Char
 import Text.Parsec.Combinator
 import Text.Parsec.String (parseFromFile, Parser(..))
@@ -15,8 +16,11 @@ import Core.Utils
 
 coreDef = haskellStyle
   { P.reservedNames = [ "let", "letrec", "case", "in", "of", "Pack" ]
-  , P.reservedOpNames = [ "=", ",", ";", "==", "~=", ">=","<=","->","<-", "\\", ".", "+","-","*","/","&","|"]
+  , P.reservedOpNames = [ "=", ",", ";","->","<-", "\\", "."]
   }
+relops = ["==", "~=", ">=","<="]
+arithops = [ "+","-","*","/"]
+bitops = ["&","|"]
 
 coreLexer   = P.makeTokenParser coreDef
 parens      = P.parens coreLexer
@@ -26,10 +30,14 @@ reserved    = P.reserved coreLexer
 reservedOp  = P.reservedOp coreLexer
 integer     = P.integer coreLexer
 symbol      = P.symbol coreLexer
+operator    = P.operator coreLexer
 spaces      = P.whiteSpace
 semi        = P.semi coreLexer
 
 int = fromIntegral <$> integer
+
+binary op = P.Infix e P.AssocLeft where
+  e = do { o <- symbol op; return (\x y -> EAp (EAp (EVar o) x) y) }
 
 -- * Parser
 
@@ -45,8 +53,25 @@ pSc = do
   body <- pCoreExpr
   return (name, args, body)
 
+data PartialExpr = NoOp | FoundOp Name CoreExpr
+
+assembleOp e1 NoOp = e1
+assembleOp e1 (FoundOp op e2) = EAp (EAp (EVar op) e1) e2
+
+tryOp opks = alternatives (map (uncurry try1) opks) <|> return NoOp
+  where try1 op k = FoundOp <$> op <*> k
+
 pCoreExpr :: Parser CoreExpr
-pCoreExpr = alternatives [pLam, pCase, pLet, pCtor, pAexpr] where
+pCoreExpr = alternatives [try pLet, try pCase, try pLam, expr1] where
+  expr1 = assembleOp <$> expr2 <*> tryOp [(string "|", expr1)]
+  expr2 = assembleOp <$> expr3 <*> tryOp [(string "&", expr2)]
+  expr3 = assembleOp <$> expr4 <*> tryOp [(alternatives (string <$> relops), expr4)]
+  expr4 = assembleOp <$> expr5 <*> tryOp [(string "+", expr4), (string "+", expr5)]
+  expr5 = assembleOp <$> expr6 <*> tryOp [(string "*", expr5), (string "/", expr6)]
+
+  -- App or single Aexpr
+  expr6 = foldl1 EAp <$> many1 (try pAexpr)
+
   pLam = do
     reservedOp "\\"
     params <- many1 identifier
