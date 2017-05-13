6dac643f28ae33b85812cfc6894ac07abda1f044
Matthew Chan <matt@themattchan.com>
2017-05-13 13:43:58 -0700

    copy utils

--- src/Core/Template.hs
+++ src/Core/Template.hs
@@ -1,3 +1,13 @@
 module Core.Template where
+
 import Core.Language
 import Core.Utils
+
+data TiState = TiState { tiStack :: TiStack
+                       , tiDump  :: TiDump
+                       , tiHeap  :: TiHeap
+                       , tiGlobals :: TiGlobals
+                       , tiStats :: TiStats
+                       }
+
+type TiStack = [Addr]
--- src/Core/Utils.hs
+++ src/Core/Utils.hs
@@ -1,7 +1,43 @@
 module Core.Utils where
+import Data.Maybe
 import Data.Monoid
-import Control.Lens.Wrapped (ala)
-import Control.Applicative
 
-alternatives :: Alternative f => [f a] -> f a
-alternatives = ala Alt foldMap
+type Addr = Int
+data Heap a = Heap { heapSize :: Int, heapFree :: [Addr], heapCts :: [(Addr,a)] }
+
+hInitial :: Heap a
+hInitial = Heap 0 [1..] []
+
+hAlloc :: Heap a -> a -> (Heap a, Addr)
+hAlloc (Heap s (next:free) cts) n = (Heap (s+1) free ((next,n):cts), next)
+hAlloc _ _ = error "hAlloc: heap full"
+
+hUpdate :: Heap a -> Addr -> a -> Heap a
+hUpdate h a n = h { heapCts = (a,n) : remove (heapCts h) a }
+
+hFree :: Heap a -> Addr -> Heap a
+hFree h a = h { heapSize = heapSize h -1
+              , heapFree = a : heapFree h
+              , heapCts  = remove (heapCts h) a
+              }
+
+hLookup :: Heap a -> Addr -> a
+hLookup h a = fromMaybe err $ lookup a (heapCts h)
+  where err = error ("can't find node " <> showAddr a <> " in heap")
+
+hAddresses :: Heap a -> [Addr]
+hAddresses = map fst . heapCts
+
+hNull :: Addr
+hNull = 0
+
+hIsNull :: Addr -> Bool
+hIsNull = (0 ==)
+
+showAddr :: Addr -> String
+showAddr a = "#" ++ show a
+
+remove :: [(Addr,a)] -> Addr -> [(Addr,a)]
+remove [] a = error ("Attempt to update or free nonexistent address " <> showAddr a)
+remove ((a',n):cts) a | a == a' = cts
+                      | a /= a' = (a',n) : remove cts a
