6c4eef8bac3b0b53c5a5446011cec9a5db7b139d
Matthew Chan <matt@themattchan.com>
2017-05-15 18:58:04 -0700

    prettyprint precedence

--- src/Core/Pretty.hs
+++ src/Core/Pretty.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PatternGuards #-}
 module Core.Pretty where
 import Text.PrettyPrint
 import Core.Language
@@ -13,37 +14,55 @@ ppProgram = map ppScDefn
 
 ppScDefn :: CoreScDefn -> Doc
 ppScDefn (name, args, expr) =
-  text name <+> ppVarList args <+> char '=' <+> ppExpr expr
+  text name <+> ppVarList args <+> char '=' <+> ppExpr 1 expr
+
+binops :: [(String, Int)]
+binops = [("*", 1), ("/", 1)
+         ,("+", 2), ("-", 2)] ++
+         map (\o -> (o,3)) [ "<", ">", "==", "~=", ">=","<="] ++
+         [("&",4)
+         ,("|",5)]
+
+parenPrec :: Bool -> (Doc -> Doc)
+parenPrec b | b          = parens
+            | otherwise  = id
 
 ppExpr :: Int -> CoreExpr -> Doc
 ppExpr _ (ENum n)    = int n
 ppExpr _ (EVar v)    = text v
 ppExpr _ (EConstr tag arity) = text "Pack" <> braces (int tag <> comma <> int arity)
+
 ppExpr prec (EAp left@(EAp (EVar b) e1) e2)
-  | b `elem` binops =
-ppExpr prec (EAp e1 e2) = ppExpr prec e1 <+> ppAExpr prec e2
+  | Just p <-  b `lookup` binops
+  = parenPrec (prec > p)
+  $ ppExpr p e1 <+> text b <+> ppExpr p e2
+
+ppExpr prec (EAp e1 e2) =
+  let (p1,d1) = ppApp prec e1
+      (p2,d2) = ppApp (prec+1) e2
+  in parenPrec (p1<p2) d1 <+> parenPrec (p2<p1) d2 -- but now everything has full parens :(
+  where
+    ppApp prec e = (prec, ppExpr prec e)
+
 ppExpr prec (ELet isRec defns expr)
   = hang (text (if isRec then "letrec" else "let"))
           3 (ppDefns defns) $+$
-    text "in " <> ppExpr expr
+    text "in " <> ppExpr prec expr
+
 ppExpr prec (ECase e alts)
   = hang caseof 2 (ppAlts alts)
     where
-      caseof = text "case" <+> ppExpr e <+> text "of"
-      ppAlts = hcat' . map ppAlter
+      caseof = text "case" <+> ppExpr prec e <+> text "of"
+      ppAlts = vcat' . map ppAlter
       ppAlter (tag, vars, expr)
         = char '<' <> int tag <> char '>'
         <+> ppVarList vars
-        <+> text "->" <+> ppExpr expr <+> semi
-ppExpr prec (ELam args body) = char '\\' <+> ppVarList args <+> char '.' <+> ppExpr body
---ppExpr e = ppAExpr e
+        <+> text "->" <+> ppExpr prec expr <+> semi
 
--- ppAExpr :: CoreExpr -> Doc
--- ppAExpr e | isAtomicExpr e = ppExpr e
---           | otherwise      = parens (ppExpr e)
+ppExpr prec (ELam args body) = char '\\' <> ppVarList args <> char '.' <+> ppExpr prec body
 
 ppDefns :: [(Name, CoreExpr)] -> Doc
-ppDefns = hcat' . punctuate semi . map ppDefn
+ppDefns = vcat' . punctuate semi . map ppDefn
 
 ppDefn :: (Name, CoreExpr) -> Doc
 ppDefn (name, expr) = ppScDefn (name, [], expr)
